a64d0f785788563b70b02fcea6b168365dfcea4b
corner <corner3499@gmail.com>
2022-12-19 17:50:20 +0900

    -

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
         <template v-if="$fetchState.pending">
             <div class="article-cards-wrapper">
                 <content-placeholders v-for="p in 30" :key="p" rounded class="article-card-block">
-                    <content-placeholders-img />
-                    <content-placeholders-text :lines="3" />
+                    <content-placeholders-img/>
+                    <content-placeholders-text :lines="3"/>
                 </content-placeholders>
             </div>
         </template>
@@ -14,8 +14,7 @@
         <template v-else>
             <div class="article-cards-wrapper">
                 <article-card-block v-for="(article, i) in articles" :key="article.id" v-observe-visibility="
-                  i === articles.length - 1 ? lazyLoadArticles : false
-                " :article="article" class="article-card-block" />
+                  i === articles.length - 1 ? lazyLoadArticles : false" :article="article" class="article-card-block"/>
             </div>
         </template>
     </div>
